8afd511f5e98b2ffd457d6a6477d66ed7e2ba46e
asnarendran99 <162266811+asnarendran99@users.noreply.github.com>
2024-10-06 23:20:30 +1100

    Update main.py

--- book_catalog/app/main.py
+++ book_catalog/app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, HTTPException, Depends, status
 from sqlalchemy.orm import Session
-from app import crud, models, schemas
-from app.database import SessionLocal, engine
-
+from .database import SessionLocal, engine
+from . import crud, models, schemas
 
 models.Base.metadata.create_all(bind=engine)
 
